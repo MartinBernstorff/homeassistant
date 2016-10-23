ae3f45236c1c2b87036f12a8a05fd472cdc709cc
Ryqiem <ryqiem@gmail.com>
2016-10-23 11:07:45 +0200

    Added inputs for sunset python

--- apps/sunset.py
+++ apps/sunset.py
@@ -2,10 +2,13 @@ import appdaemon.appapi as appapi
 import datetime
 
 #
-# Weekend_sleeper app
+# Sunset app
 #
 # Args:
-#
+#   switch: The switch that initializes the script
+#   factor: the input_select that determines the factor length
+#   hour: the hour to start sunset
+#   minutes: the minutor to start sunset
 
 class Sunset(appapi.AppDaemon):
 
@@ -22,5 +25,26 @@ class Sunset(appapi.AppDaemon):
         # Schedule a daily callback that will call run_daily() at 12am every midday
         self.run_every(self.sunset, time, 30, **kwargs)
 
+        #Reset the switch at 08:00 each day
+        time = datetime.time(8, 0, 0)
+        self.run_daily(self.reset, time)
+
     def sunset(self, kwargs):
         self.log("The sun is setting!")
+
+    def updatefactor(self, entity="", attribute="", old="", new="", kwargs=""):
+        self.factor_state = self.get_state(self.args["factor"])
+
+        if self.factor_state == "50%":
+            self.modulator = 0.5
+        elif self.factor_state == "75%":
+            self.modulator = 0.75
+        elif self.factor_state == "100%":
+            self.modulator = 1
+        elif self.factor_state == "125%":
+            self.modulator = 1.25
+
+        self.log("Modulator set to " + str(self.modulator))
+
+    def reset(self, entity="", attribute="", old="", new="", kwargs=""):
+        self.turn_off(self.args["switch"])
