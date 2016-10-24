d6d2185a82af9ed156f5da650f3a83648f4eb510
Martin <ryqiem@gmail.com>
2016-10-24 21:36:48 +0200

    Bugfixes

--- apps/circadian_single_lights.py
+++ apps/circadian_single_lights.py
@@ -17,9 +17,9 @@ class CircadianSingleLights(appapi.AppDaemon):
         switch = self.args["switch"]
 
         #Setup callback
-        time = datetime.time(21, 15, 0)
+        time = datetime.time(21, 16, 0)
         self.run_daily(self.ceiling, time)
 
     def ceiling(self, entity="", attribute="", old="", new="", kwargs=""):
-        if self.get_state(switch) == "on":
+        if self.get_state(self.args["switch"]) == "on":
             self.turn_off("light.loft")
