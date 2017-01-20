b4b441fda4e6bf5fedd9bd71e6bd7a9ff259bddd
Martin <ryqiem@gmail.com>
2017-01-20 13:28:46 +0100

    Implemented c_offset and added fade

--- apps/off_scheduler.py
+++ apps/off_scheduler.py
@@ -10,20 +10,51 @@ import datetime
 
 class off_scheduler(appapi.AppDaemon):
 
-    def initialize(self):
-        switch = self.args["switch"]
-        hour = self.args["hour"]
-        minute = self.args["minute"]
-        entity = self.args["entity"]
-        time = datetime.time(int(hour), int(minute), 0)
+    def initialize(self, entity="", attribute="", old="", new="", kwargs=""):
 
-        self.log("Initializing OffScheduler with switch: {}, {}:{}, {}".format(switch, hour, minute, entity))
-        self.run_daily(self.off, time)
+        self.switch = self.args["switch"]
+        self.hour = self.args["hour"]
+        self.minute = self.args["minute"]
+        self.entity = self.args["entity"]
+
+        self.init = self.datetime().replace(hour=int(self.hour), minute=int(self.minute), second=0)
+        offset = self.global_vars["c_offset"]
+        run_at = (self.init + offset).time()
+
+        self.log("Initializing OffScheduler with params: \n          switch: {}\n          run_at: {}\n          entity: {}".format(self.switch, run_at, self.entity))
+
+
+        self.handle = self.run_daily(self.off, run_at)
+
+        self.listen_state(self.update_time, "input_select.circadian_hour")
+        self.listen_state(self.update_time, "input_select.circadian_minute")
 
 
     def off(self, entity="", attribute="", old="", new="", kwargs=""):
+        device, entity = self.split_entity(self.entity)
+
         if self.args["switch"] is not None:
             if self.get_state(self.args["switch"]):
-                self.turn_off(self.args["entity"])
+                if device == "light":
+                    self.light_off()
+                else:
+                    self.turn_off(self.args["entity"])
         else:
-            self.turn_off(self.args["entity"])
+            if device == "light":
+                self.light_off()
+            else:
+                self.turn_off(self.args["entity"])
+
+    def light_off(self):
+            self.turn_on(self.entity, transition = 30, xy_color = self.global_vars["c_colortemp"], brightness = 0)
+            time.sleep(30)
+            self.turn_off(self.entity)
+
+    def update_time(self, entity="", attribute="", old="", new="", kwargs=""):
+        self.cancel_timer(self.handle)
+
+        offset = self.global_vars["c_offset"]
+        run_at = (self.init + offset).time()
+
+        self.handle = self.run_daily(self.off, run_at)
+        self.log("Initializing OffScheduler with params: \n          switch: {}\n          run_at: {}\n          entity: {}".format(self.switch, run_at, self.entity))
